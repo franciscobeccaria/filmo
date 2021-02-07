dfd266b330e016e42bf246db23d2419c88d99cbd
Francisco Beccaria <fbeccaria24@gmail.com>
2021-02-06 22:32:12 -0300

    Commit Fixed Scrollbar on Firefox

--- src/MovieSlider.jsx
+++ src/MovieSlider.jsx
@@ -39,6 +39,9 @@ const Slider = styled.div`
         -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
         background-color: #555;
     }
+
+    scrollbar-color: #000 #FFFFFF2E;
+    scrollbar-width: thin;
 `
 const Title = styled.h4`
     text-align: center;
